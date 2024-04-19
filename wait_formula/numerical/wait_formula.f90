3c8471c238e49cb6844fb7801a1ba69725e3493a
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2024-04-19 08:11:50 +0330

    Update wait_formula.f90

--- wait_formula/numerical/wait_formula.f90
+++ wait_formula/numerical/wait_formula.f90
@@ -1,43 +1,29 @@
 program wait_formula
     implicit none
     
-    real :: beta, a, h
-    real, dimension(:), allocatable :: rho
+    real :: beta, a, h, rho, rho_step, pi, infinity
     complex :: norm_deltaZ
     complex :: integral1, integral2, integral3
-    real :: rho_start, rho_end, rho_step
-    real :: rho_current
-    integer :: i
-    
+
+    pi = 3.14
     beta = 2.0 * 3.14 / 100.0
     a = 18.0
     h = 20.0
-    
-    rho_start = a
-    rho_end = 1000.0
+    infinity = 1000
+    rho = a
     rho_step = 0.01
-    
-    allocate(rho((rho_end - rho_start) / rho_step + 1))
-    
-    rho_current = rho_start
-    do i = 1, size(rho)
-        rho(i) = rho_current
-        rho_current = rho_current + rho_step
-    end do
     
     integral1 = 0.0
     integral2 = 0.0
     integral3 = 0.0
-    do i = 1, size(rho)
-        integral1 = integral1 + (exp(-2.0 * cmplx(0.0, 1.0) * beta * sqrt(rho(i)**2 + h**2)) / rho(i)) * rho_step
-        integral2 = integral2 + (exp(-beta * cmplx(0.0, 1.0) * (sqrt(rho(i)**2 + h**2) + rho(i))) / rho(i)) * rho_step
-        integral3 = integral3 + (exp(-2.0 * cmplx(0.0, 1.0) * beta * rho(i)) / rho(i)) * rho_step
+    do while (rho <= infinity)
+        rho = rho + rho_step
+        integral1 = integral1 + (exp(-2.0 * cmplx(0.0, 1.0) * beta * sqrt(rho**2 + h**2)) / rho) * rho_step
+        integral2 = integral2 + (exp(-beta * cmplx(0.0, 1.0) * (sqrt(rho**2 + h**2) + rho)) / rho) * rho_step
+        integral3 = integral3 + (exp(-2.0 * cmplx(0.0, 1.0) * beta * rho) / rho) * rho_step
     end do
     
     norm_deltaZ = (-2.0 / (sin(beta * h)**2)) * (integral1 - 2.0 * cos(beta * h) * integral2 - cos(beta * h)**2 * integral3)
-    
-    print *, "Result:", norm_deltaZ
-    
-    deallocate(rho)
+    print *, "Normalized ∆Z:", norm_deltaZ
     
 end program wait_formula
