13b74eb0e879e0e538da96ca2980e2cfba5a8b9d
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2025-08-10 12:38:16 +0330

    Create V2Power.m

---
+++ V/V2Power.m
@@ -0,0 +1,12 @@
+
+V = 10.0;
+R_r = 50.0;
+X = 25.0;
+
+I_mag = V / sqrt(R_r^2 + X^2);
+phi = atan(X / R_r);
+
+I_real = I_mag * cos(phi)
+I_reac = I_mag * sin(phi)
+
+P_rad = 0.5 * (I_mag ^ 2) * R_r
